a0c00aceba947863ece0278d7b90539f880396e0
e <547252920@qq.com>
2018-03-29 16:58:56 +0800

    添加头部,用css实现tab动画

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
 window.onload = function () {
-    moveIn();
-}
-
-//右侧nav移入动画
-function moveIn() {
-    var oDiv = document.getElementById('rightNav');
-    var oUl = oDiv.getElementsByTagName('ul');
-    //console.log(oUl);
-    [].forEach.call(oUl, function (e) {
-        e.addEventListener('mouseenter', function (event) {
-            var target = event.target;
-            if (target.tagName.toLowerCase() === 'li') {
-                console.log(target)
-            }
-        },true)
-    })
-
+    
 }
